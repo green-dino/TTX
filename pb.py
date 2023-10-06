98a9e9e3dc4637920b1803a211e474cb9fac17cc
green-dino <104174275+green-dino@users.noreply.github.com>
2023-10-06 16:52:45 -0700

    playbook updates

--- pb.py
+++ pb.py
@@ -1,12 +1,5 @@
 from utils.playbook_blocks import Block
-from utils.playbook_functions import Function, node_labels
+from utils.playbook_functions import Function, node_labels, node
 from utils.playbook_nodes import node_data
 from utils.playbook_tasks import Task
 from utils.playbook_relationships import relationships
-
-print (Task)
-
-print(relationships)
-
-
-print(type(node_data))
